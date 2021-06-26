cef72e9f89fd372150e2ca133473f96a2a8bde54
meczx <86465757+meczx@users.noreply.github.com>
2021-06-26 18:37:31 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+function Info() {
+  return(
+    <div className="note">
+     <h1> Javascript And React js</h1>
+     <p>Web development is a easy thing</p>
+    </div>
+  );
+}
+
+export default Info;
